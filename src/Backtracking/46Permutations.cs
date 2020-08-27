d4f13da413d93fe4f572802ee506aef99971fbec
David Gu <macdavid313@gmail.com>
2020-08-27 17:18:57 +1000

    [46] improved

--- src/Backtracking/46Permutations.cs
+++ src/Backtracking/46Permutations.cs
@@ -3,8 +3,8 @@
  * Project: Backtracking
  * Created Date: Thursday, 27th August 2020 2:20:59 pm
  * Author: David Gu (macdavid313@gmail.com)
- * Runtime: 240 ms, faster than 94.14% of C# online submissions for Permutations.
- * Memory Usage: 31.2 MB, less than 90.93% of C# online submissions for Permutations.
+ * Runtime: 236 ms, faster than 98.01% of C# online submissions for Permutations.
+ * Memory Usage: 31.2 MB, less than 91.59% of C# online submissions for Permutations.
  * Copyright (c) David Gu 2020
  */
 
@@ -18,52 +18,71 @@ namespace Permutations
     {
         public IList<IList<int>> Permute(int[] nums)
         {
-            var stack = new Stack<int>(nums.Length);
-            var totalLen = Factorial(nums.Length);
-            IList<IList<int>> permutations = new List<IList<int>>(totalLen);
-            var indexCache = new HashSet<int>(nums.Length);
-            for (var i = 0; i < nums.Length; i++)
-            {
-                stack.Push(nums[i]);
-                for (var j = 0; j < nums.Length; j++)
-                    indexCache.Add(j);
-                indexCache.Remove(i);
-                Backtracking(permutations, stack, nums, indexCache);
-                stack.Pop();
-            }
+            int totalLen = Factorial(nums.Length);
+            var permutations = new List<IList<int>>(totalLen);
+            Span<int> numsCopy = stackalloc int[nums.Length];
+            nums.CopyTo(numsCopy);
+            Backtrack(nums.Length, permutations, numsCopy, 0);
             return permutations;
+        }
+
+        void Backtrack(int n, List<IList<int>> permutations, Span<int> permutation, int offset)
+        {
+            if (offset == n)
+            {
+                permutations.Add(permutation.ToArray());
+                return;
+            }
+            for (var i = offset; i < n; i++)
+            {
+                int tmp;
+                // swap
+                if (i != offset)
+                {
+                    tmp = permutation[offset];
+                    permutation[offset] = permutation[i];
+                    permutation[i] = tmp;
+                }
+                // backtrack
+                Backtrack(n, permutations, permutation, offset + 1);
+                // undo swap
+                if (i != offset)
+                {
+                    tmp = permutation[offset];
+                    permutation[offset] = permutation[i];
+                    permutation[i] = tmp;
+                }
+            }
         }
 
         int Factorial(int n)
         {
             if (n < 0)
                 throw new InvalidOperationException();
-            int res = 1;
-            while (n != 0)
+            if (n <= 10)
+            {
+                return n switch
+                {
+                    0 => 1,
+                    1 => 1,
+                    2 => 2,
+                    3 => 6,
+                    4 => 24,
+                    5 => 120,
+                    6 => 720,
+                    7 => 5040,
+                    8 => 40320,
+                    9 => 362800,
+                    _ => 3628000
+                };
+            }
+            int res = 3628000;
+            while (n != 10)
             {
                 res *= n;
                 n -= 1;
             }
             return res;
-        }
-
-        void Backtracking(IList<IList<int>> permutations, Stack<int> permutation, int[] nums, HashSet<int> otherIdx)
-        {
-            if (otherIdx.Count == 0)
-            {
-                permutations.Add(permutation.ToArray());
-                return;
-            }
-            var otherIdxArray = new int[otherIdx.Count];
-            otherIdx.CopyTo(otherIdxArray);
-            foreach (var idx in otherIdxArray)
-            {
-                permutation.Push(nums[idx]);
-                otherIdx.Remove(idx);
-                Backtracking(permutations, permutation, nums, otherIdx);
-                otherIdx.Add(idx);
-                permutation.Pop();
-            }
         }
     }
 }
