206682d6348f805f07d169b5bb2c12cdb66d0989
David Gu <macdavid313@gmail.com>
2020-09-04 08:20:28 +1000

    [257] submitted

---
+++ src/Tree/257BinaryTreePaths.cs
@@ -0,0 +1,62 @@
+/*
+ * File: 257BinaryTreePaths.cs
+ * Project: Tree
+ * Created Date: Friday, 4th September 2020 7:27:05 am
+ * Author: David Gu (macdavid313@gmail.com)
+ * Runtime: 248 ms, faster than 87.79% of C# online submissions for Binary Tree Paths.
+ * Memory Usage: 31.7 MB, less than 32.47% of C# online submissions for Binary Tree Paths.
+ * Copyright (c) David Gu 2020
+ */
+
+
+using System.Collections.Generic;
+
+namespace BinaryTreePaths
+{
+    public class Solution
+    {
+        public IList<string> BinaryTreePaths(TreeNode root)
+        {
+            if (root is null) return new string[0];
+            if (root.left is null && root.right is null) return new string[1] { root.val.ToString() };
+            var path = new List<string>();
+            var allPaths = new List<string>();
+            Travel(root, path, allPaths);
+            return allPaths;
+        }
+
+        void Travel(TreeNode node, List<string> path, List<string> allPaths)
+        {
+            path.Add(node.val.ToString());
+            if (node.left is null && node.right is null)
+            {
+                allPaths.Add(PathToString(path));
+                path.RemoveAt(path.Count - 1);
+                return;
+            }
+            if (node.left is object)
+                Travel(node.left, path, allPaths);
+            if (node.right is object)
+                Travel(node.right, path, allPaths);
+            path.RemoveAt(path.Count - 1);
+        }
+
+        string PathToString(List<string> path) => string.Join("->", path);
+    }
+
+
+    /* Definition for a binary tree node. */
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
---
+++ src/Tree/README.md
@@ -0,0 +1,14 @@
+# Tree
+
+Problems categorised into **Tree**.
+
+|  ID   |                                 Name                                  | Difficulty |
+| :---: | :-------------------------------------------------------------------: | :--------: |
+|  257  | [Binary Tree Paths](https://leetcode.com/problems/binary-tree-paths/) |    Easy    |
+
+## References
+
+* LeetCode, [https://leetcode.com/tag/tree/](https://leetcode.com/tag/tree/)
+* Wikipedia, Tree, [https://en.wikipedia.org/wiki/Tree_(data_structure)](https://en.wikipedia.org/wiki/Tree_(data_structure))
+* Wikipedia Binary Search Tree, [https://en.wikipedia.org/wiki/Binary_search_tree](https://en.wikipedia.org/wiki/Binary_search_tree)
+* Wikipedia, B-Tree, [https://en.wikipedia.org/wiki/B-tree](https://en.wikipedia.org/wiki/B-tree)
